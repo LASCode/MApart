ef2e1b1856d0e93a4a81810f0c1938a4fbfb3853
DmitryGuseatnicov <78113027+DmitryGuseatnicov@users.noreply.github.com>
2023-06-01 14:11:46 +0300

    photo (#10)

--- src/constants/gallery/index.ts
+++ src/constants/gallery/index.ts
@@ -1,15 +1,15 @@
 export const galleryPhotos = [
     "https://images2.imgbox.com/8d/35/zsgy6h6f_o.jpg",
-    "https://i.ibb.co/MVBYndZ/room-blue-4.jpg",
+    "https://images2.imgbox.com/58/2b/eWYp9URY_o.jpg",
     "https://images2.imgbox.com/8b/ed/egNo5NxC_o.jpg",
-    "https://i.ibb.co/FhwKdY3/room-green-1.jpg",
+    "https://images2.imgbox.com/b0/3d/NiN7QpCr_o.jpg",
     "https://images2.imgbox.com/de/f4/0hI94O6W_o.jpg",
-    "https://i.ibb.co/BfgSh4y/room-green.jpg",
+    "https://images2.imgbox.com/eb/f9/76CMhaCu_o.jpg",
 
-    "https://i.ibb.co/QQ0djDY/room-green-6.jpg",
+    "https://images2.imgbox.com/e0/af/mqvlYpdQ_o.jpg",
     "https://images2.imgbox.com/09/4e/JDjTDuU5_o.jpg",
-    "https://i.ibb.co/CvG2JWV/room-yellow-4.jpg",
+    "https://images2.imgbox.com/8c/69/1uPNnic9_o.jpg",
     "https://images2.imgbox.com/99/49/r6HCcoLa_o.jpg",
-    "https://i.ibb.co/RH5gY1n/room-grey-3.jpg",
-    "https://i.ibb.co/RSpc0gv/room-yellow.jpg",
+    "https://images2.imgbox.com/8a/1a/M7u0Rwbr_o.jpg",
+    "https://images2.imgbox.com/0a/cd/5S5ZgCGz_o.jpg",
 ];
